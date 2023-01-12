008f205b655b06d9e61276b5f8525308f4330673
Zahrasadeg <sadeghiz071@gmail.com>
2023-01-11 22:02:36 -0500

    v1

---
+++ dowhileloop/src/class1.java
@@ -0,0 +1,8 @@
+public class class1 {
+    public static void main(String[] args) {
+        int num=0;
+        while (num<10){
+            System.out.println(num);
+        num++;}
+    }
+}
---
+++ dowhileloop/src/forloops.java
@@ -0,0 +1,19 @@
+public class forloops {
+    public static void main(String[] args) {
+        //print num 1-9
+        int a=0;
+        while(a<10){
+            System.out.println(a);
+            a++;
+        }
+        System.out.println("********************");
+        for(int i=0;i<10;i++) {
+            System.out.println(i);
+        }
+            System.out.println("&&&&&&&&&&&&&&&&&&&&");
+            for(int y=1;y<20;y+=2)
+                System.out.println(y);
+
+
+    }
+}
